1082e1d1ecb7e8ca61ced5167670a2067c31180b
Gabriele <gabrielepersola@gmail.com>
2024-04-05 10:57:26 +0200

    Aggiornamento responsive, hamburger menu non ancora funzionante

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    menu.classList.toggle("active");
+}
+
+/*
+function toggleMenu() {
+    var menu = document.querySelector(".menu");
+    menu.classList.toggle("active");
+}
+*/
