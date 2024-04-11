b2baf7c0d296c664753e0fc1bd6dff263228c3f5
Gabriele <gabrielepersola@gmail.com>
2024-04-11 12:44:39 +0200

    sistemazione responsive pagina contatti

--- script.js
+++ script.js
@@ -1,11 +1,68 @@
 function toggleMenu() {
-    var menu = document.getElementById("menu");
+    var menu = document.getElementById("hm");
     menu.classList.toggle("active");
 }
 
 /*
 function toggleMenu() {
-    var menu = document.querySelector(".menu");
+    var menu = document.querySelector(".hamburger-menu");
     menu.classList.toggle("active");
 }
 */
+
+
+/* FORM */
+
+document.getElementById("myForm").addEventListener("submit", function(event) {
+    var nameInput = document.getElementById("nome");
+    var surnameInput = document.getElementById("cognome");
+    var emailInput = document.getElementById("email");
+
+    var nameError = document.getElementById("name-error");
+    var surnameError = document.getElementById("surname-error");
+    var emailError = document.getElementById("email-error");
+
+    // Controllo validità campo nome
+    if (nameInput.value === "") {
+        nameError.style.display = "block";
+        nameInput.classList.add("error");
+        event.preventDefault(); // Impedisce l'invio del form
+    } else {
+        nameError.style.display = "none";
+        nameInput.classList.remove("error");
+    }
+
+    // Controllo validità campo cognome
+    if (surnameInput.value === "") {
+        surnameError.style.display = "block";
+        surnameInput.classList.add("error");
+        event.preventDefault(); // Impedisce l'invio del form
+    } else {
+        surnameError.style.display = "none";
+        surnameInput.classList.remove("error");
+    }
+
+    // Controllo validità campo email
+    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(emailInput.value)) {
+        emailError.style.display = "block";
+        emailInput.classList.add("error");
+        event.preventDefault(); // Impedisce l'invio del form
+    } else {
+        emailError.style.display = "none";
+        emailInput.classList.remove("error");
+    }
+});
+
+// Aggiungi un listener per controllare l'input quando l'utente modifica il campo email
+emailInput.addEventListener("input", function() {
+    var emailError = document.getElementById("email-error");
+    if (emailPattern.test(emailInput.value)) {
+        emailError.style.display = "none";
+        emailInput.classList.remove("error");
+    } else {
+        emailError.style.display = "block";
+        emailInput.classList.add("error");
+    }
+});
+
